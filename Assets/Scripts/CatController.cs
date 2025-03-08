5f7e0c99498dbfcdc4aeaa7863fa97b5b84cc7c3
Nathan Zweifel <33692087+zweifelna@users.noreply.github.com>
2025-03-08 16:30:10 +0100

    add cat state machine

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -3,28 +3,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class CatController : MonoBehaviour
 {
-    Vector2 catPosition;
-    public Vector2 catSpeed = new Vector2(-1,0);
+    private enum CatState { Neutral, Interested, Bored } // États du chat
+    private CatState currentState = CatState.Neutral;    // État actuel
 
-    // Start is called before the first frame update
+    private Vector2 catPosition;
+    public Vector2 catSpeed = new Vector2(-1, 0);
+
     void Start()
     {
-        this.catPosition = this.gameObject.transform.position;
+        this.catPosition = this.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        this.transform.position = catPosition;
+        this.transform.position = catPosition; // Met à jour la position du chat
 
-        if(Input.GetKey(KeyCode.A)){
-            Debug.Log("A appuyé");
-            catPosition += catSpeed;
-        } 
-        
+        HandleState(); // Gère l'état actuel du chat
+    }
+
+    private void HandleState()
+    {
+        switch (currentState)
+        {
+            case CatState.Neutral:
+                if (Input.GetKey(KeyCode.A))
+                {
+                    Debug.Log("Chat devient intéressé !");
+                    currentState = CatState.Interested;
+                }
+                break;
+
+            case CatState.Interested:
+                if (Input.GetKey(KeyCode.A))
+                {
+                    catPosition += catSpeed; // Déplacement
+                }
+                else
+                {
+                    Debug.Log("Chat se lasse...");
+                    currentState = CatState.Bored;
+                }
+                break;
+
+            case CatState.Bored:
+                if (Input.GetKey(KeyCode.A))
+                {
+                    Debug.Log("Chat reprend de l'intérêt !");
+                    currentState = CatState.Interested;
+                }
+                break;
+        }
     }
 }
