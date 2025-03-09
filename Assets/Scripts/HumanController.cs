a877617de9021d36ed3ed36f0800c1db8c3f7142
Limonello <leonie.courbat@etu.hesge.ch>
2025-03-09 15:27:42 +0100

    State machine pour l'humain

---
+++ Assets/Scripts/HumanController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanController : MonoBehaviour
+{
+    private enum HumanState { Neutral, Pstpst, Catch } // États de l'humain
+    private HumanState currentState = HumanState.Neutral;    // État actuel
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleState(); // Gère l'état actuel du chat
+    }
+
+    private void HandleState()
+    {
+        switch (currentState)
+        {
+            case HumanState.Neutral:
+                if (Input.GetKey(KeyCode.A))
+                {
+                    currentState = HumanState.Pstpst;
+                    Debug.Log("Neutre");
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    currentState = HumanState.Catch;
+                    Debug.Log("Catch");
+                }
+                break;
+
+            case HumanState.Pstpst:
+                if (Input.GetKey(KeyCode.A))
+                {
+                    Debug.Log("Pstpst");
+                }
+                else if (Input.GetKeyUp(KeyCode.A))
+                {
+                    currentState = HumanState.Neutral;
+                    Debug.Log("Go neutre");
+                }
+                break;
+
+            case HumanState.Catch:
+                if (Input.GetKey(KeyCode.D))
+                {
+                    Debug.Log("Catching");
+                }
+                else if (Input.GetKeyUp(KeyCode.D))
+                {
+                    currentState = HumanState.Neutral;
+                    Debug.Log("Go neutre");
+                }
+                break;
+        }
+    }
+}
