01b68e816e9d15567c22665faf1be33f6d762010
Limonello <leonie.courbat@etu.hesge.ch>
2025-03-10 15:02:32 +0100

    Cat is now walking

--- Assets/Scripts/ChangeSprite.cs
+++ Assets/Scripts/ChangeSprite.cs
@@ -10,20 +10,29 @@ public class ChangeSprite : MonoBehaviour
     public Sprite interestedCat;
     public Sprite boredCat;
 
+    public Animator animationsCat;
+
     // Start is called before the first frame update
     void Start()
     {
         this.gameObject.GetComponent<SpriteRenderer>().sprite = interestedCat;
+        animationsCat = GetComponent<Animator>();
+        animationsCat.Play("cat_bored_still");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(cat.currentState == CatController.CatState.Bored)
+        if (cat.currentState == CatController.CatState.Bored)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = boredCat;
-        }else if (cat.currentState == CatController.CatState.Interested){
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = interestedCat;
+            //this.gameObject.GetComponent<SpriteRenderer>().sprite = boredCat;
+            animationsCat.Play("cat_bored_still");
+
+        }
+        else if (cat.currentState == CatController.CatState.Interested)
+        {
+            //this.gameObject.GetComponent<SpriteRenderer>().sprite = interestedCat;
+            animationsCat.Play("cat_walking");
         }
     }
 }
